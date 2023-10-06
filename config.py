38381395afa546365bae98b0577c39892bf3c1d1
Sandra Calzada <127830026+sndr157@users.noreply.github.com>
2023-10-06 16:52:10 +0200

    config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 BOUGHT_CSV_PATH = 'data/bought.csv'
 SOLD_CSV_PATH = "data/sold.csv"
+DATE_PATH = "current_date.txt"
 
-with open("current_date.txt", 'r') as f:
-	CURRENT_DATE = f.read()
+with open(DATE_PATH, 'r') as f:
+	CURRENT_DATE = f.read().strip()
